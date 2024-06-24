696add60a201b683a9ccb5591671a47603c5b38d
ShavadzeGio <giorgishavadze101@gmail.com>
2024-06-24 17:54:42 +0400

    first commit

---
+++ level 18/level 18.py
@@ -0,0 +1,13 @@
+
+collages = ["goa", "novator", "it academy step", "smart academy", "Other..."]
+print(collages)
+user_input = input("Wich academy do you study?")
+for i in collages:
+    if user_input == "goa":
+        print("You are CHAD")
+        break
+    else:
+        print("You are VIRGIN")
+        break
+
+
---
+++ level 18/level18cw.py
@@ -0,0 +1,5 @@
+cars = ["subaru", "toyota", "mitsubishi", "nissan"]
+
+lenght = len(cars)
+
+print(cars[lenght - 3])
